e489e83bb2b48eb79deb5594c84816ede05b08ce
gopika~ <gopikaks.cse2020@citchennai.net>
2023-09-12 18:51:03 +0530

    changes

---
+++ src/main/resources/static/data.js
@@ -0,0 +1,23 @@
+const data = document.getElementById("data")
+console.log("data script is working");
+let arr = [];
+axios.get("http://localhost:8080/get-forms")
+    .then((res) => {
+        console.log(arr);
+        arr = res.data;
+        showdata();
+    })
+
+function showdata() {
+    console.log(arr);
+    arr.forEach((val, index) => {
+        console.log(index);
+        data.innerHTML += `
+        <div style="border: 2px solid black">
+        <h1><span>${index + 1}. Name :  ${val.name}</span></h1>
+
+        <h1>&nbsp;Email: ${val.email}</h1>
+    </div>
+        `;
+    })
+}
